bb227ca6211493c485fd23d395643b35de82b88b
vinnyk <ragdolleye@gmail.com>
2021-06-18 10:44:15 -0500

    Adds comparison between Pikaday and vue2-datepicker in Calendar.vue

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -3,6 +3,16 @@
     <h1>{{ title }}</h1>
     <div>
       <input type="text" id="datepicker" />
+      <p>{{ pikaTime }}</p>
+    </div>
+    <div>
+      <h1>{{ titleDatepicker }}</h1>
+      <date-picker v-model="time1" valueType="format"></date-picker>
+      <date-picker v-model="time2" type="datetime"></date-picker>
+      <date-picker v-model="time3" range></date-picker>
+      <p>Time 1: {{ time1 }}</p>
+      <p>Time 2: {{ time2 }}</p>
+      <p>Time 3: {{ time3 }}</p>
     </div>
   </div>
 </template>
@@ -10,16 +20,27 @@
 <script>
 import Pikaday from "pikaday";
 import "pikaday/css/pikaday.css";
+import DatePicker from "vue2-datepicker";
+import "vue2-datepicker/index.css";
 
 export default {
+  components: { DatePicker },
   data: function () {
     return {
-      title: "I'm The Calendar!",
+      title: "I'm The Pikaday Calendar!",
+      titleDatepicker: "I'm the Vue 2 Date Picker!",
+      time1: null,
+      time2: null,
+      time3: null,
+      pikaTime: null,
     };
   },
 
   mounted: function () {
-    var picker = new Pikaday({ field: document.getElementById("datepicker") });
+    var picker = new Pikaday({
+      field: document.getElementById("datepicker"),
+      format: "YYYY-M-D",
+    });
     console.log(picker);
   },
 };
