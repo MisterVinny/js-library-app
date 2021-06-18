01c496e843489b64ef409b7ffb542683b030926a
vinnyk <ragdolleye@gmail.com>
2021-06-18 09:56:00 -0500

    initial commit, adds calendar vue and basic functionality.

---
+++ src/views/Calendar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="calendar">
+    <h1>{{ title }}</h1>
+    <div>
+      <input type="text" id="datepicker" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Pikaday from "pikaday";
+import "pikaday/css/pikaday.css";
+
+export default {
+  data: function () {
+    return {
+      title: "I'm The Calendar!",
+    };
+  },
+
+  mounted: function () {
+    var picker = new Pikaday({ field: document.getElementById("datepicker") });
+    console.log(picker);
+  },
+};
+</script>
